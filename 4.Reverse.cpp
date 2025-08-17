3ee0e4f6e5b6714d8322f94978fbb382a4693325
Tasnim Sultana Oishi <sultanatasnim205@gmail.com>
2025-08-17 18:38:11 +0600

    Add files via upload

---
+++ 4.Reverse.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cout << "Enter number of elements: ";
+    cin >> n;
+
+    int arr[100];
+    cout << "Enter " << n << " elements: ";
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    cout << "Reversed array: ";
+    for (int i = n - 1; i >= 0; i--) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
